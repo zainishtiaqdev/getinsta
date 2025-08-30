425568dda2ffbd0e276a31c75bd8121021472608
Zain Ishtiaq <zainishtiaqahmad61@gmail.com>
2025-08-30 17:39:58 +0500

    tesrses

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const outputDir = path.resolve("./downloads");
 if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
